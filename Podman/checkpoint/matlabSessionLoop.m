b3f566d22cc2ec5f89a8a0689dcb8ac4295e8357
Ahmed Hassan <ahmedh@mathworks.com>
2025-11-13 00:16:48 -0500

    Renaming the session file

---
+++ Podman/checkpoint/matlabSessionLoop.m
@@ -0,0 +1,67 @@
+%   Copyright 2025 The MathWorks, Inc.
+function matlabSessionLoop(exitSessionMarker)
+    arguments
+        exitSessionMarker = "exitSession";
+    end
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    %               Initialization
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    disp('Starting MATLAB session initialization...')
+
+    %------- inbound & outbound folders --------%
+    if ispc
+        homePath = getenv('USERPROFILE');
+    else
+        homePath = getenv('HOME');
+    end
+
+    loopRootPath = fullfile(homePath, ".matlab");
+    inboundPath = fullfile(loopRootPath, ".inbound");
+    outboundPath = fullfile(loopRootPath, ".outbound");
+    archivePath = fullfile(loopRootPath, ".archive");
+    for p=[inboundPath, outboundPath, archivePath]
+        if ~isfolder(p)
+            mkdir(p);
+        end
+    end
+
+    %------- warming up toolboxes --------%
+    new_system('a')
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    %               Waiting for commands
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    disp('Waiting for commands...')
+    EOF_MARKER = "###############--EOF_EXECUTION--###############";
+    while true
+        entries = dir(inboundPath);
+        files = entries(~[entries.isdir]); % remove directories
+        if ~isempty(files)
+            [~, idx] = min([files.datenum]);
+            oldest_in_file = files(idx).name;
+            [~, oldest_file_name, ~] = fileparts(oldest_in_file);
+            oldest_out_file = [oldest_file_name '.out'];
+            try
+                file_content = fileread(fullfile(inboundPath, oldest_in_file));
+                if strcmp(file_content, exitSessionMarker)
+                    disp('...Exiting the session loop');
+                    diary(fullfile(outboundPath, oldest_out_file));
+                    movefile(fullfile(inboundPath, oldest_in_file), fullfile(archivePath, oldest_in_file));
+                    disp(EOF_MARKER);
+                    diary off;
+                    break;
+                end
+                disp(strjoin(['Executing:', fullfile(inboundPath, oldest_in_file)]));
+                diary(fullfile(outboundPath, oldest_out_file));
+                movefile(fullfile(inboundPath, oldest_in_file), fullfile(archivePath, oldest_in_file));
+                eval(file_content);
+            catch ME
+                disp(strjoin(["Error identifier:", ME.identifier]));
+                disp(ME.getReport());
+            end
+            disp(EOF_MARKER);
+            diary off;
+        else
+            pause(0.2);
+        end
+    end
+end
--- Podman/dockerfiles/matlab-ci-ready.Dockerfile
+++ Podman/dockerfiles/matlab-ci-ready.Dockerfile
@@ -3,7 +3,7 @@ ARG BASE_IMAGE=matlab_ci:r2024b_oct25
 
 FROM ${BASE_IMAGE}
 
-COPY checkpoint/matlabSessionInit.m matlabSessionInit.m
+COPY checkpoint/matlabSessionLoop.m .
 COPY checkpoint/matlab-bs .
 COPY checkpoint/matlab-bs.ps1 .
 COPY checkpoint/matlab-bs.py .
