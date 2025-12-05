1c88fa1c6f4622e5f6e8bf5cb981189e64b309ce
Ahmed Hassan <ahmedh@mathworks.com>
2025-12-05 15:31:06 -0500

    Doc enhancements

--- Podman/2_Building_MATLAB_non-interactive_24b.sh
+++ Podman/2_Building_MATLAB_non-interactive_24b.sh
@@ -2,16 +2,20 @@
 #           Building MATLAB non-interactive Image
 #####################################################################
 
-# chmod +x *
-export IMAGE_NAME="slcicd.azurecr.io/slcheck/matlab_ci"
+export CONTAINER_REGISTRY=""            # e.g.: slcicd.azurecr.io/slcheck
+export IMAGE_NAME="${CONTAINER_REGISTRY:+$CONTAINER_REGISTRY/}matlab_ci"
 export IMAGE_TAG="r2024b_oct25"
 export IMAGE_FULLNAME="$IMAGE_NAME:$IMAGE_TAG"
 export CI_IMAGE_TAG="r2024b_oct25_ready"
 export CI_IMAGE_FULLNAME="$IMAGE_NAME:$CI_IMAGE_TAG"
 
+#####################################
+#  Build the standard MATLAB image
+#####################################
+# cd <wsl_matlab_repo>/Podman/
 export MATLAB_RELEASE="R2024b"
 export PRODUCTS="MATLAB Simulink Simulink_Check Simulink_Design_Verifier Simulink_Report_Generator Simulink_Coder Simulink_Compiler Simulink_Test Embedded_Coder Simulink_Coverage Requirements_Toolbox CI/CD_Automation_for_Simulink_Check"
-# cd ~/wsl_matlab/Podman/
+
 sudo podman build -f dockerfiles/non-interactive.Dockerfile \
     --build-arg MATLAB_RELEASE="$MATLAB_RELEASE" \
     --build-arg MATLAB_PRODUCT_LIST="$PRODUCTS" \
@@ -19,12 +23,16 @@ sudo podman build -f dockerfiles/non-interactive.Dockerfile \
 
 # sudo podman push "$IMAGE_FULLNAME"
 
+#####################################
+#  Add checkpointing helper files
+#####################################
 sudo sed -i 's/\r$//' imagefiles/matlab-bs*
+
 sudo podman build -f dockerfiles/matlab-ci-ready.Dockerfile \
     --build-arg BASE_IMAGE="$IMAGE_FULLNAME" \
     -t "$CI_IMAGE_FULLNAME" .
 
-# cd ~/wsl_matlab/Podman/
+# cd <wsl_matlab_repo>/Podman/
 # sudo apt-get install yq -y
 # sudo podman login slcicd.azurecr.io -u slcicd -p "$(yq -r '.settings.secrets.docker_registry_credentials' ./../env.yml)"
 # sudo podman pull "$CI_IMAGE_FULLNAME"
--- Podman/3_Testing_checkpoint.sh
+++ Podman/3_Testing_checkpoint.sh
@@ -1,9 +1,11 @@
+# cd <wsl_matlab_repo>/Podman/
+
 export IMAGE_NAME="slcicd.azurecr.io/slcheck/matlab_ci"
 export CI_IMAGE_TAG="r2024b_oct25_ready"
 export CI_IMAGE_FULLNAME="$IMAGE_NAME:$CI_IMAGE_TAG"
 export CHECKPOINT_IMAGE="matlab_ci_checkpoint_img:v1"
 # sudo apt-get install yq -y
-# cd ~/wsl_matlab/Podman
+# You might need to retrieve the batch license token in a different way in the variable below
 export MLM_LICENSE_TOKEN="$(yq '.settings.secrets.mlm_license_token' ./../env.yml)"
 ##############################################
 #    Clean up previous containers/images
@@ -18,14 +20,15 @@ sudo podman ps -a
 #    Creating checkpoint (#-Export  #-compress=none)
 ##############################################
 cd ~
-mkdir /tmp/ws_root
 # Starting a standard MATLAB container
 export cp_container_name="container_to_checkpoint"
+export sharedVolume="/tmp/ws_root"
+mkdir $sharedVolume
 time sudo podman run -d \
         --network=none \
         --name $cp_container_name \
         -e MLM_LICENSE_TOKEN=$MLM_LICENSE_TOKEN \
-        -v /tmp/ws_root:/tmp/ws_root \
+        -v $sharedVolume:$sharedVolume \
         $CI_IMAGE_FULLNAME matlab-batch "matlabSessionLoop();"
 echo "--------------------------------------- Container has been created"
 time sudo podman exec $cp_container_name matlab-bs-wait-start
@@ -37,32 +40,33 @@ echo "--------------------------------------- Container is ready"
 time sudo podman container checkpoint --compress=none --export=checkpoint_dump.tar $cp_container_name
 echo "--------------------------------------- Checkpoint has been exported"
 
-
 ##############################################
 #    Warmup a dummy container - required for new machines
 ##############################################
-cd ~
-export cp_dummy="dummy_container"
-time {
-        sudo podman run -d \
-                --network=none \
-                --name $cp_dummy \
-                alpine sleep infinity
-        time sudo podman container checkpoint --compress=none --export=checkpoint_dump_small.tar $cp_dummy
-        sudo podman stop -t0 $(sudo podman ps -aq)
-        sudo podman rm -f $(sudo podman ps -aq)
-        sudo podman ps -a
-        
+sudo podman stop -t0 $(sudo podman ps -aq)
+sudo podman rm -f $(sudo podman ps -aq)
+sudo podman ps -a
+
+# cd ~
+# export cp_dummy="dummy_container"
+# time {
+#         sudo podman run -d \
+#                 --network=none \
+#                 --name $cp_dummy \
+#                 alpine sleep infinity
+#         time sudo podman container checkpoint --compress=none --export=checkpoint_dump_small.tar $cp_dummy
+#         sudo podman stop -t0 $(sudo podman ps -aq)
+#         sudo podman rm -f $(sudo podman ps -aq)
+#         sudo podman ps -a
 # }
 ##############################################
 #    Restoring checkpoint
 ##############################################
 cd ~
-mkdir /tmp/ws_root
+mkdir $sharedVolume
 
 export cp_container_test="container_test_export"
 time sudo podman container restore --import=checkpoint_dump.tar --name $cp_container_test
-#time sudo podman container restore --import=checkpoint_dump_bak.tar --name $cp_container_test
 echo "--------------------------------------- Checkpoint has been restored"
 sudo podman exec $cp_container_test matlab-bs -batch "disp('-------------------------');"
 sudo podman exec $cp_container_test matlab-bs -batch "disp('test___1');disp('test___2');"
